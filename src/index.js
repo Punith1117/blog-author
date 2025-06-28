cbfe9e55979f5cf8e4ef6a995d4257fac15d7575
Punith1117 <punith11172006@gmail.com>
2025-06-28 23:32:43 +0530

    add initial render logic as function to render login and header
    This occurs only when there's no jwt token in the local storage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
+import { header } from "./components/header";
+import { login } from "./components/login";
 import "./reset.css";
 import "./style.css";
+import { getJwtFromStorage } from "./utilities";
+
+export const initialRenderLogic = () => {
+    let body = document.querySelector('body')
+    if (getJwtFromStorage() == null) {
+        body.replaceChildren(header(), login())
+    }
+}
+
+initialRenderLogic()
