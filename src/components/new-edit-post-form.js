72ab159bf8652706c8a8023810d3d76191951f50
Punith1117 <punith11172006@gmail.com>
2025-06-29 23:23:18 +0530

    add component to display form to edit or create post

---
+++ src/components/new-edit-post-form.js
@@ -0,0 +1,58 @@
+export const newEditPostForm = (formData) => {
+    let form = document.createElement('form')
+    form.className = 'post'
+        let heading = document.createElement('h1')
+        heading.textContent = 'New post'
+        let titleLabel = document.createElement('label')
+        titleLabel.textContent = 'Title: '
+            let title = document.createElement('input')
+            title.type = 'text'
+            title.required = 'true'
+        titleLabel.appendChild(title)
+        let contentLabel = document.createElement('label')
+        contentLabel.textContent = 'Content: '
+            let content = document.createElement('textarea')
+            content.required = 'true'
+        contentLabel.appendChild(content)
+        let options = document.createElement('div')
+        options.textContent = 'Publish Status: '
+            let trueOptionLabel = document.createElement('label')
+            trueOptionLabel.textContent = 'True'
+                let trueOption = document.createElement('input')
+                trueOption.type = 'radio'
+                trueOption.name = 'isPublished'
+                trueOption.value = true
+            trueOptionLabel.appendChild(trueOption)
+            let falseOptionLabel = document.createElement('label')
+            falseOptionLabel.textContent = 'False'
+                let falseOption = document.createElement('input')
+                falseOption.type = 'radio'
+                falseOption.name = 'isPublished'
+                falseOption.value = false
+            falseOptionLabel.appendChild(falseOption)
+        options.appendChild(trueOptionLabel)
+        options.appendChild(falseOptionLabel)
+        let cancelButton = document.createElement('button')
+        cancelButton.textContent = 'Cancel'
+        cancelButton.type = 'button'
+        let submitButton = document.createElement('button')
+        submitButton.textContent = 'Submit'
+        submitButton.type = 'submit'
+        if (formData != undefined) {
+            form.dataset.postId = formData.id
+            heading.textContent = 'Edit Post'
+            title.value = formData.title
+            content.value = formData.content
+            if (formData.isPublished == true)
+                trueOption.checked = true
+            else
+                falseOption.checked = true
+        }
+    form.appendChild(heading)
+    form.appendChild(titleLabel)
+    form.appendChild(contentLabel)
+    form.appendChild(options)
+    form.appendChild(cancelButton)
+    form.appendChild(submitButton)
+    return form
+}
