18ee4f8f366174c4c837cd3d86dfafe2a913ba73
Punith1117 <punith11172006@gmail.com>
2025-06-29 22:11:18 +0530

    add event listener to navigate between published and unpublished posts
    The event listener also contains a callback that redirects to posts display

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,13 @@
+import { handlePostsDisplay } from "../component-display-handlers/posts"
+
 export const nav = () => {
     const navbar = document.createElement('nav')
         const publishedPostsButton = document.createElement('button')
+        publishedPostsButton.addEventListener('click', () => handlePostsDisplay(true))
         publishedPostsButton.textContent = 'Published Posts'
         const unpublishedPostsButton = document.createElement('button')
         unpublishedPostsButton.textContent = 'Unpublished Posts'
+        unpublishedPostsButton.addEventListener('click', () => handlePostsDisplay(false))
         const newPostButton = document.createElement('button')
         newPostButton.textContent = 'New'
     navbar.appendChild(publishedPostsButton)
