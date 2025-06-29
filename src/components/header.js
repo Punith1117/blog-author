a77ddc4904fbb1d98faaa26491d232063b64697e
Punith1117 <punith11172006@gmail.com>
2025-06-29 19:30:34 +0530

    modify function to display user greeting with username sent in parameter

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,18 @@
-export let header = () => {
+export let header = (username) => {
     let header = document.createElement('header')
         let h1 = document.createElement('h1')
         h1.textContent = 'Blog Author'
         let websiteAuthor = document.createElement('a')
         websiteAuthor.href = 'https://github.com/Punith1117/blog-author'
         websiteAuthor.textContent = 'By Punith1117'
+        let userGreeting
+        if (username != undefined) {
+            userGreeting = document.createElement('h2')
+            userGreeting.textContent = 'Welcome, ' + username
+        }
     header.appendChild(h1)
     header.appendChild(websiteAuthor)
+    if (username != undefined)
+        header.appendChild(userGreeting)
     return header
 }
