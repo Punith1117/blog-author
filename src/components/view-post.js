56fb5761e4fc22b1a5a5aeae2878066a0a87a3fd
Punith1117 <punith11172006@gmail.com>
2025-06-30 19:47:59 +0530

    add event listeners with callback to all utility buttons
    Edit, Delete and Back

--- src/components/view-post.js
+++ src/components/view-post.js
@@ -1,3 +1,7 @@
+import { handleDeletePost } from "../component-display-handlers/delete-post"
+import { handleNewEditPost } from "../component-display-handlers/new-edit-post"
+import { handlePostsDisplay } from "../component-display-handlers/posts"
+
 export const viewPost = (post) => {
     let main = document.createElement('main')
     main.dataset.id = post.id
@@ -10,10 +14,13 @@ export const viewPost = (post) => {
         let utilities = document.createElement('div')
             let editButton = document.createElement('button')
             editButton.textContent = 'edit'
+            editButton.addEventListener('click', () => handleNewEditPost(post.id))
             let deleteButton = document.createElement('button')
             deleteButton.textContent = 'delete'
+            deleteButton.addEventListener('click', () => handleDeletePost(post.id, post.isPublished))
             let backButton = document.createElement('button')
             backButton.textContent = 'back'
+            backButton.addEventListener('click', () => handlePostsDisplay(post.isPublished))
         utilities.appendChild(editButton)
         utilities.appendChild(deleteButton)
         utilities.appendChild(backButton)
