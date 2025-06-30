9be8e2b013fe00f41b5510c5103b118da190aeef
Punith1117 <punith11172006@gmail.com>
2025-06-30 17:13:20 +0530

    add component to display single post

---
+++ src/components/view-post.js
@@ -0,0 +1,24 @@
+export const viewPost = (post) => {
+    let main = document.createElement('main')
+    main.dataset.id = post.id
+        let title = document.createElement('h2')
+        title.textContent = post.title
+        let content = document.createElement('p')
+        content.textContent = post.content
+        let publishStatus = document.createElement('p')
+        publishStatus.textContent = 'Publish Status: ' + post.isPublished
+        let utilities = document.createElement('div')
+            let editButton = document.createElement('button')
+            editButton.textContent = 'edit'
+            let deleteButton = document.createElement('button')
+            deleteButton.textContent = 'delete'
+            let backButton = document.createElement('button')
+            backButton.textContent = 'back'
+        utilities.appendChild(editButton)
+        utilities.appendChild(deleteButton)
+        utilities.appendChild(backButton)
+    main.appendChild(title)
+    main.appendChild(content)
+    main.appendChild(utilities)
+    return main
+}
