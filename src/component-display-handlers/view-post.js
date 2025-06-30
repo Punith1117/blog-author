a6bf604a76f261c505a40a97b023f70b7521e4c1
Punith1117 <punith11172006@gmail.com>
2025-06-30 17:14:00 +0530

    add handler to display of single post

---
+++ src/component-display-handlers/view-post.js
@@ -0,0 +1,20 @@
+import { getPostQuery } from "../apiQueries"
+import { header } from "../components/header"
+import { nav } from "../components/nav"
+import { viewPost } from "../components/view-post"
+import { getJwt, getUsername } from "../utilities"
+import { handleLoginDisplay } from "./login"
+
+export const handleViewPost = async (id) => {
+    const jwt = getJwt()
+    let post
+    try {
+        const object = await getPostQuery(jwt, id)
+        post = object.posts
+    } catch (e) {
+        if (e.message == 401)
+            handleLoginDisplay('Session expired after 2min. Login again')
+    }
+    let body = document.querySelector('body')
+    body.replaceChildren(header(getUsername()), nav(), viewPost(post))
+}
