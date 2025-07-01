a47f534117bc203f97a4e81854fd77be6585061c
Punith1117 <punith11172006@gmail.com>
2025-07-01 12:54:05 +0530

    add logout button with its callback

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,6 @@
+import { handleLoginDisplay } from "../component-display-handlers/login"
+import { destroyJwt, destroyUsername } from "../utilities"
+
 export let header = (username) => {
     let header = document.createElement('header')
         let h1 = document.createElement('h1')
@@ -6,13 +9,25 @@ export let header = (username) => {
         websiteAuthor.href = 'https://github.com/Punith1117/blog-author'
         websiteAuthor.textContent = 'By Punith1117'
         let userGreeting
+        let logoutButton
         if (username != undefined) {
             userGreeting = document.createElement('h2')
             userGreeting.textContent = 'Welcome, ' + username
+            logoutButton = document.createElement('button')
+            logoutButton.addEventListener('click', () => handleLogOut())
+            logoutButton.textContent = 'Logout'
         }
     header.appendChild(h1)
     header.appendChild(websiteAuthor)
-    if (username != undefined)
+    if (username != undefined){
         header.appendChild(userGreeting)
+        header.appendChild(logoutButton)
+    }
     return header
+}
+
+const handleLogOut = async () => {
+    destroyJwt()
+    destroyUsername()
+    await handleLoginDisplay()
 }
