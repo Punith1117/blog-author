e839a2632afe631b8d9d47fdc2f6fc88b86b56cc
Punith1117 <punith11172006@gmail.com>
2025-07-01 13:53:44 +0530

    add event listeners to signup and cancel button with callback

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,5 +1,13 @@
-export const signup = () => {
+import { signupQuery } from "../apiQueries"
+import { handleLoginDisplay } from "../component-display-handlers/login"
+import { header } from "./header"
+
+export const signup = (err) => {
     const form = document.createElement('form')
+        const error = document.createElement('p')
+        if (error != undefined) {
+            error.textContent = err
+        }
         const heading = document.createElement('h3')
         heading.textContent = 'Sign up'
         const usernameLabel = document.createElement('label')
@@ -17,13 +25,30 @@ export const signup = () => {
         const cancelButton = document.createElement('button')
         cancelButton.type = 'button'
         cancelButton.textContent = 'Cancel'
+        cancelButton.addEventListener('click', () => handleLoginDisplay())
         const submitButton = document.createElement('button')
         submitButton.type = 'submit'
         submitButton.textContent = 'Submit'
+    form.addEventListener('submit', (e) => handleSignup(e))
     form.appendChild(heading)
+    form.appendChild(error)
     form.appendChild(usernameLabel)
     form.appendChild(passwordLabel)
     form.appendChild(cancelButton)
     form.appendChild(submitButton)
     return form
+}
+
+const handleSignup = async (e) => {
+    e.preventDefault()
+    const form = e.target
+    const username = form.elements['username'].value
+    const password = form.elements['password'].value
+    try {
+        await signupQuery(username, password)
+        await handleLoginDisplay()
+    } catch (e) {
+        const body = document.querySelector('body')
+        body.replaceChildren(header(), signup(e.message))
+    }
 }
